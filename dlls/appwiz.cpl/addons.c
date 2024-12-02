3fd38fb5337497df405872daf3724e1fa8c1ac32
pipetto-crypto <pipetto-crypto@gmail.com>
2024-12-02 16:56:04 +0100

    HACK: Force x86_64 gecko and mono on arm64

--- dlls/appwiz.cpl/addons.c
+++ dlls/appwiz.cpl/addons.c
@@ -48,8 +48,8 @@ WINE_DEFAULT_DEBUG_CHANNEL(appwizcpl);
 #ifdef __i386__
 #define GECKO_ARCH "x86"
 #define GECKO_SHA "26cecc47706b091908f7f814bddb074c61beb8063318e9efc5a7f789857793d6"
-#elif defined(__x86_64__)
-#define GECKO_ARCH "x86_64"
+#elif defined(__x86_64__) || defined(__aarch64__)
+#define GECKO_ARCH "x86_64" 
 #define GECKO_SHA "e590b7d988a32d6aa4cf1d8aa3aa3d33766fdd4cf4c89c2dcc2095ecb28d066f"
 #else
 #define GECKO_ARCH ""
@@ -57,7 +57,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(appwizcpl);
 #endif
 
 #define MONO_VERSION "9.3.0"
-#if defined(__i386__) || defined(__x86_64__)
+#if defined(__i386__) || defined(__x86_64__) || defined(__aarch64__)
 #define MONO_ARCH "x86"
 #define MONO_SHA "6ca2c0aed096ff9ec20faf5eb76c6b7d7de82d9a8875ac7dd9f0793bf9c3fd30"
 #else
