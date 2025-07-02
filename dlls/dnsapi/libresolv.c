3b925ed2412e70611479a6bfcda0ad6cc0fb8396
pipetto-crypto <pipetto-crypto@gmail.com>
2025-07-02 09:54:07 +0200

    revert commit 0510de7

--- dlls/dnsapi/libresolv.c
+++ dlls/dnsapi/libresolv.c
@@ -62,7 +62,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(dnsapi);
 
 #ifdef __ANDROID__
 
-#define LIBANDROID_HANDLE "/system/lib64/libandroid.so"
+#define LIBANDROID_NAME "libandroid.so"
 
 #include <dlfcn.h>
 enum ResNsendFlags : uint32_t {
@@ -81,7 +81,7 @@ static int (*p_android_res_nresult)(int fd, int *rcode, uint8_t *answer, size_t 
 
 static void init_resolver( void ) {
 	if (!libandroid_handle)
-		libandroid_handle = dlopen(LIBANDROID_HANDLE, RTLD_LOCAL | RTLD_NOW);
+		libandroid_handle = dlopen(LIBANDROID_NAME, RTLD_GLOBAL | RTLD_NOW);
     if (!p_android_res_nquery)
     	p_android_res_nquery = dlsym(libandroid_handle, "android_res_nquery");
     if (!p_android_res_nresult)
