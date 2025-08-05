c37b9a2d259b381d6ea5ef999f8476470d4669fb
TGP-17 <mytablet19048@gmail.com>
2025-08-05 05:29:41 +0200

    Create clang-wrapper.sh

---
+++ clang-wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+REAL_CLANG="/usr/local/lib/android/sdk/ndk/27.2.12479018/toolchains/llvm/prebuilt/linux-x86_64/bin/clang"
+
+# Filter out -mabi=ms
+ARGS=()
+for arg in "$@"; do
+    if [[ "$arg" != "-mabi=ms" ]]; then
+        ARGS+=("$arg")
+    fi
+done
+
+# Call the real clang with modified args
+exec "$REAL_CLANG" "${ARGS[@]}"
